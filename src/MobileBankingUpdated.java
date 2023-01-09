2b1bb2caad200344991d44368ad63c608f33492d
Maaz Bin Hossain <mhossainsaki@gmail.com>
2023-01-09 12:36:45 +0600

    minor changes

--- src/MobileBankingUpdated.java
+++ src/MobileBankingUpdated.java
@@ -1,12 +1,8 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
 import java.io.*;
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 class Users{
     String phoneNumber;
